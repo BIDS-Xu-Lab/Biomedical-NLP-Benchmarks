de102c58a40be492503475546dd1c7cf238254c0
Yan(Sawyer) Hu <550359540@qq.com>
2024-04-25 13:19:57 -0500

    Add files via upload

--- llama/scripts/run-NER.sh
+++ llama/scripts/run-NER.sh
@@ -9,6 +9,6 @@ for dataset in "${datasets[@]}"; do
     # Loop over each model
     for model in "${models[@]}"; do
         # Run the Python script with the current dataset and model
-        CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 29502 src/multiple_GPU_train_V2.py -d "$dataset" -m "$model"
+        CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 29502 src/NER/multiple_GPU_train_V2-NER.py -d "$dataset" -m "$model"
     done
 done
--- llama/scripts/run-RE.sh
+++ llama/scripts/run-RE.sh
@@ -9,6 +9,6 @@ for dataset in "${datasets[@]}"; do
     # Loop over each model
     for model in "${models[@]}"; do
         # Run the Python script with the current dataset and model
-        CUDA_VISIBLE_DEVICES=1,3,5,6,7 accelerate launch --main_process_port 29501 src/multiple_GPU_train_V2-RE.py -d "$dataset" -m "$model"
+        CUDA_VISIBLE_DEVICES=1,3,5,6,7 accelerate launch --main_process_port 29501 src/RE/multiple_GPU_train_V2-RE.py -d "$dataset" -m "$model"
     done
 done
